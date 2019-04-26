d39eac68a586103652a727302298a1f3e21b1ccf
Jagadeesh Kumar Natesan <jag.psg@gmail.com>
2019-04-26 18:48:03 +0530

    Folder structure change

--- ProjectManagerUI/src/app/filtertask.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FiltertaskPipe } from './filtertask.pipe';
-
-describe('FiltertaskPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FiltertaskPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
